28fccc9b09a9aa5bcb69e33a946b584a21ca3ae8
z-Zoey <b00408044@g.ntu.edu.tw>
2020-09-28 16:01:22 +0800

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG','') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shielded-tundra-96636.herokuapp.com','127.0.0.1']
 
 
 # Application definition
